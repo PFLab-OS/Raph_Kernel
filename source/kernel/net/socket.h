04eb9be2e7b2974bb2fbca4967cd861512a259a8
Fukumu Tsutsumi <levelfourslv@gmail.com>
2016-03-27 08:09:48 +0000

    mp #1 use ARPSocket

--- source/kernel/net/socket.h
+++ source/kernel/net/socket.h
@@ -155,8 +155,8 @@ public:
   virtual int32_t ReceivePacket(uint16_t type, uint32_t *spa = nullptr, uint8_t *sha = nullptr);
   virtual void SetIPAddr(uint32_t ipaddr);
 
-  static const uint16_t kOpARPRequest = 0x0001;
-  static const uint16_t kOpARPReply = 0x0002;
+  static const int16_t kOpARPRequest = 0x0001;
+  static const int16_t kOpARPReply = 0x0002;
 };
 
 #endif // __RAPH_KERNEL_NET_SOCKET_H__
