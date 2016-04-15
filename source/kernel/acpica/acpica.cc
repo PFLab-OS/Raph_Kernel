0c924263fde94d2523b3cfcba0106b86fdbedb84
Yuki Imai <yuki08281993@gmail.com>
2016-04-15 08:46:18 +0000

    clean Makefil of acpica

--- source/kernel/acpica/acpica.cc
+++ source/kernel/acpica/acpica.cc
@@ -4,14 +4,28 @@ extern "C"{
 }
 #include "acpica.h"
 
+
+#define __ACPICA_DEBUG__
+
+#ifdef __ACPICA_DEBUG__
+#include <stdio.h>
+#define DEBUG(str)   (puts(str))
+#else
+#define DEBUG(str) 
+#endif
+
+
 void Acpica::Init() {
   ACPI_STATUS status;
+  DEBUG("InitializeSubsytem segufo here.");
   AcpiInitializeSubsystem();
-  AcpiTerminate();
-
+  DEBUG("Initialize tables");
   AcpiInitializeTables(NULL, 16, FALSE);
+  DEBUG("load tables.");
   AcpiLoadTables();
+  DEBUG("Enable Subsystem");
   AcpiEnableSubsystem(0);
+  DEBUG("Init Obj");
   AcpiInitializeObjects(0);
 }
 
