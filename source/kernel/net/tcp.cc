0533a8f2ffc4beea2809ed3a2debde577dd5c926
Fukumu Tsutsumi <levelfourslv@gmail.com>
2016-05-03 09:33:50 +0000

    tcp header option (mss,ws)

--- source/kernel/net/tcp.cc
+++ source/kernel/net/tcp.cc
@@ -40,6 +40,44 @@ static const uint8_t kWindowSizeOffset  = 14;
 /*
  * TCP header option parameters
  */
+struct TcpOptionEol {
+  uint8_t number;
+} __attribute__((packed));
+
+struct TcpOptionNop {
+  uint8_t number;
+} __attribute__((packed));
+
+struct TcpOptionMss {
+  uint8_t number;
+  uint8_t length;
+  uint16_t mss;
+} __attribute__((packed));
+
+struct TcpOptionWindowScale {
+  uint8_t number;
+  uint8_t length;
+  uint8_t ws;
+} __attribute__((packed));
+
+struct TcpOptionSackPermitted {
+  uint8_t number;
+  uint8_t length;
+} __attribute__((packed));
+
+struct TcpOptionSack {
+  uint8_t number;
+  uint8_t length;
+  uint32_t seq;
+} __attribute__((packed));
+
+struct TcpOptionTimeStamp {
+  uint8_t number;
+  uint8_t length;
+  uint32_t ts;
+  uint32_t ecr;
+} __attribute__((packed));
+
 // option number
 static const uint8_t kOptionEndOfOptionList = 0;
 static const uint8_t kOptionNoOperation     = 1;
@@ -48,6 +86,7 @@ static const uint8_t kOptionWindowScale     = 3;
 static const uint8_t kOptionSackPermitted   = 4;
 static const uint8_t kOptionSack            = 5;
 static const uint8_t kOptionTimeStamp       = 8;
+
 // option length
 static const uint8_t kOptionLength[] = {
   0x01,
@@ -63,24 +102,58 @@ static const uint8_t kOptionLength[] = {
 
 uint16_t CheckSum(uint8_t *buf, uint32_t size, uint32_t saddr, uint32_t daddr);
 
-int32_t TcpGenerateHeader(uint8_t *buffer, uint32_t length, uint32_t saddr, uint32_t daddr, uint16_t sport, uint16_t dport, uint8_t type, uint32_t seq, uint32_t ack) {
+int32_t TcpGenerateHeader(uint8_t *buffer, uint32_t length, uint32_t saddr, uint32_t daddr, uint16_t sport, uint16_t dport, uint8_t type, uint32_t seq, uint32_t ack, struct TcpOptionParameters *options) {
+  uint32_t option_offset = sizeof(TcpHeader);
+
+  // generate option fields
+  if(options) {
+    if((type & Socket::kFlagSYN) && options->mss != TcpOptionParameters::kIgnore) {
+      TcpOptionMss * volatile s = reinterpret_cast<TcpOptionMss*>(buffer + option_offset);
+      s->number = kOptionMSS;
+      s->length = kOptionLength[kOptionMSS];
+      s->mss = options->mss;
+      option_offset += kOptionLength[kOptionMSS];
+    }
+
+    if((type & Socket::kFlagSYN) && options->ws != TcpOptionParameters::kIgnore) {
+      TcpOptionWindowScale * volatile s = reinterpret_cast<TcpOptionWindowScale*>(buffer + option_offset);
+      s->number = kOptionWindowScale;
+      s->length = kOptionLength[kOptionWindowScale];
+      s->ws = options->ws;
+      option_offset += kOptionLength[kOptionWindowScale];
+    }
+
+    TcpOptionEol * volatile s = reinterpret_cast<TcpOptionEol*>(buffer + option_offset);
+    s->number = kOptionEndOfOptionList;
+    option_offset += kOptionLength[kOptionEndOfOptionList];
+
+    if(option_offset % 4) {
+      // 4B-alignment padding
+      for(uint16_t i = 1; i < 4 - option_offset % 4; i++) {
+        buffer[option_offset + i] = 0;
+      }
+    }
+  }
+
+  // generate main fields
   TcpHeader * volatile header = reinterpret_cast<TcpHeader*>(buffer);
+  uint16_t header_length = option_offset;
   header->sport = htons(sport);
   header->dport = htons(dport);
   header->seq_number = htonl(seq);
   header->ack_number = htonl(ack);
-  header->header_len = (sizeof(TcpHeader) >> 2) << 4;
+  header->header_len = (header_length >> 2) << 4;
   header->flag = type;
-  header->window_size = 0;
+  header->window_size = 0xffff;
   header->checksum = 0;
   header->urgent_pointer = 0;
 
-  header->checksum = CheckSum(reinterpret_cast<uint8_t*>(header), length, saddr, daddr);
+  header->checksum = CheckSum(reinterpret_cast<uint8_t*>(header), header_length + length, saddr, daddr);
 
-  return 0;
+  return header_length;
 }
 
-bool TcpFilterPacket(uint8_t *packet, uint16_t sport, uint16_t dport, uint8_t type, uint32_t seq, uint32_t ack) {
+bool TcpFilterPacket(uint8_t *packet, uint16_t sport, uint16_t dport, uint8_t type, uint32_t seq, uint32_t ack, struct TcpOptionParameters *options) {
   TcpHeader * volatile header = reinterpret_cast<TcpHeader*>(packet);
 
   // TODO: temporary ignore push flag
