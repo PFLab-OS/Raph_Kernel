9403987390690c38575c5536c776ed994f3c477c
liva <sap.pcmail@gmail.com>
2016-05-11 07:54:59 +0000

    file split

--- source/kernel/task.h
+++ source/kernel/task.h
@@ -20,12 +20,13 @@
  * 
  */
 
-#ifndef __RAPH_KERNEL_DEV_TASK_H__
-#define __RAPH_KERNEL_DEV_TASK_H__
+#ifndef __RAPH_KERNEL_TASK_H__
+#define __RAPH_KERNEL_TASK_H__
 
 #include <global.h>
 #include <spinlock.h>
 #include <mem/virtmem.h>
+#include <function.h>
 
 class Task;
 
@@ -58,173 +59,6 @@ class TaskCtrl {
 
     TaskQueueState state;
   } *_task_struct = nullptr;
-};
-
-class FunctionBaseObj {
-public:
-  FunctionBaseObj() {
-  }
-  FunctionBaseObj(const FunctionBaseObj &f) {
-  }
-  virtual ~FunctionBaseObj() {
-  }
-  void Execute() {
-    if (CanExecute()) {
-      ExecuteSub();
-    }
-  }
-  virtual bool CanExecute() {
-    return false;
-  }
-  virtual void Clear() {
-  }
-  virtual FunctionBaseObj *Duplicate() const {
-    return virtmem_ctrl->New<FunctionBaseObj>(*this);
-  }
-protected:
-  virtual void ExecuteSub() {
-  }
-};
-
-class FunctionObj : public FunctionBaseObj {
-public:
-  FunctionObj() {
-  }
-  FunctionObj(const FunctionObj &f) {
-    _func = f._func;
-    _arg = f._arg;
-  }
-  virtual ~FunctionObj() {
-  }
-  void Init(void (*func)(void *), void *arg) {
-    _func = func;
-    _arg = arg;
-  }
-  virtual bool CanExecute() override {
-    return (_func != nullptr);
-  }
-  virtual void Clear() override {
-    _func = nullptr;
-  }
-  virtual FunctionBaseObj *Duplicate() const override {
-    return virtmem_ctrl->New<FunctionObj>(*this);
-  }
-private:
-  virtual void ExecuteSub() override {
-    _func(_arg);
-  }
-  void (*_func)(void *) = nullptr;
-  void *_arg;
-};
-
-template <class T>
-class ClassFunctionObj : public FunctionBaseObj {
-public:
-  ClassFunctionObj() {
-  }
-  ClassFunctionObj(const ClassFunctionObj<T> &f) {
-    _c = f._c;
-    _func = f._func;
-    _arg = f._arg;
-  }
-  virtual ~ClassFunctionObj() {
-  }
-  void Init(T *c, void (T::*func)(void *), void *arg) {
-    _c = c;
-    _func = func;
-    _arg = arg;
-  }
-  virtual bool CanExecute() override {
-    return (_func != nullptr);
-  }
-  virtual void Clear() override {
-    _func = nullptr;
-  }
-  virtual FunctionBaseObj *Duplicate() const override {
-    return virtmem_ctrl->New<ClassFunctionObj<T>>(*this);
-  }
-private:
-  virtual void ExecuteSub() override {
-    (_c->*_func)(_arg);
-  }
-  T *_c;
-  void (T::*_func)(void *) = nullptr;
-  void *_arg;
-};
-
-class GenericFunction {
-public:
-  GenericFunction() {
-  }
-  virtual ~GenericFunction() {
-  }
-  void Execute() {
-    GetObj()->Execute();
-  }
-  bool CanExecute() {
-    return GetObj()->CanExecute();
-  }
-  void Clear() {
-    GetObj()->Clear();
-  }
-  virtual FunctionBaseObj *GetObj() const = 0;
-};
-class FunctionBase : public GenericFunction {
-public:
-  FunctionBase() {
-    _obj = virtmem_ctrl->New<FunctionBaseObj>();
-  }
-  virtual ~FunctionBase() {
-    virtmem_ctrl->Delete<FunctionBaseObj>(_obj);
-  }
-  void Copy(const GenericFunction &obj) {
-    // TODO Lockをかけるべき？
-    virtmem_ctrl->Delete<FunctionBaseObj>(_obj);
-    _obj = obj.GetObj()->Duplicate();
-  }
-private:
-  FunctionBase(const FunctionBase &obj);
-  virtual FunctionBaseObj *GetObj() const override {
-    return _obj;
-  }
-  FunctionBaseObj *_obj;
-};
-class Function : public GenericFunction {
-public:
-  Function() {
-    _obj = virtmem_ctrl->New<FunctionObj>();
-  }
-  virtual ~Function() {
-    virtmem_ctrl->Delete<FunctionObj>(_obj);
-  }
-  void Init(void (*func)(void *), void *arg) {
-    _obj->Init(func, arg);
-  }
-private:
-  Function(const Function &obj);
-  virtual FunctionBaseObj *GetObj() const override {
-    return _obj;
-  }
-  FunctionObj *_obj;
-};
-template <class T>
-class ClassFunction : public GenericFunction {
-public:
-  ClassFunction() {
-    _obj = virtmem_ctrl->New<ClassFunctionObj<T>>();
-  }
-  virtual ~ClassFunction() {
-    virtmem_ctrl->Delete<ClassFunctionObj<T>>(_obj);
-  }
-  void Init(T *c, void (T::*func)(void *), void *arg) {
-    this->_obj->Init(c, func, arg);
-  }
-private:
-  ClassFunction(const ClassFunction<T> &obj);
-  virtual FunctionBaseObj *GetObj() const override {
-    return _obj;
-  }
-  ClassFunctionObj<T> *_obj;
 };
 
 class Task {
