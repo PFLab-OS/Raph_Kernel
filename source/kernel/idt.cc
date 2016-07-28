b9140104a3c4c6c226be1c68cfa7957cb62c5c93
liva <sap.pcmail@gmail.com>
2016-07-28 07:29:13 +0000

    fix #19

--- source/kernel/idt.cc
+++ source/kernel/idt.cc
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2015 Project Raphine
+ * Copyright (c) 2015 Raphine Project
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
