184cfbf389445e5940e3bb7337b86974e4871b70
liva <sap.pcmail@gmail.com>
2016-12-14 01:30:27 +0000

    add new benchmark

--- source/kernel/main.cc
+++ source/kernel/main.cc
@@ -297,6 +297,36 @@ static inline void sync(volatile int &l1, volatile int &l2, volatile int &l3) {
   } while(!__sync_bool_compare_and_swap(&l3, cpunum, cpunum));
 }
 
+static inline void sync2(int cpunum, volatile int &l1, volatile int &l2, volatile int &l3) {
+  l2 = 0;
+  while(true) {
+    int tmp = l1;
+    if (__sync_bool_compare_and_swap(&l1, tmp, tmp + 1)) {
+      break;
+    }
+  }
+  do {
+  } while(!__sync_bool_compare_and_swap(&l1, cpunum, cpunum));
+  l3 = 0;
+  while(true) {
+    int tmp = l2;
+    if (__sync_bool_compare_and_swap(&l2, tmp, tmp + 1)) {
+      break;
+    }
+  }
+  do {
+  } while(!__sync_bool_compare_and_swap(&l2, cpunum, cpunum));
+  l1 = 0;
+  while(true) {
+    int tmp = l3;
+    if (__sync_bool_compare_and_swap(&l3, tmp, tmp + 1)) {
+      break;
+    }
+  }
+  do {
+  } while(!__sync_bool_compare_and_swap(&l3, cpunum, cpunum));
+}
+
 struct Sync {
   static const int kSubStructNum = 37;
   static const int kPhysAvailableCoreNum = 32;
@@ -416,6 +446,108 @@ struct Sync2 {
   SyncSub2 third_level_lock2[kSubStructNum*2];
   SyncSub2 third_level_lock3[kSubStructNum*2];
   void Init() {
+    top_level_lock1 = 0;
+    top_level_lock2 = 0;
+    for (int i = 0; i < kSubStructNum; i++) {
+      second_level_lock1[i].Init();
+      second_level_lock2[i].Init();
+      second_level_lock3[i].Init();
+    }
+  }
+  void Do() {
+    uint32_t apicid = cpu_ctrl->GetCpuId().GetApicId();
+    if (apicid % 8 == 0) {
+      top_level_lock2 = 0;
+      while(true) {
+        int tmp = top_level_lock1;
+        if (__sync_bool_compare_and_swap(&top_level_lock1, tmp, tmp + 1)) {
+          break;
+        }
+      }
+      do {
+      } while(!__sync_bool_compare_and_swap(&top_level_lock1, kPhysAvailableCoreNum, kPhysAvailableCoreNum));
+      top_level_lock3 = 0;
+      while(true) {
+        int tmp = top_level_lock2;
+        if (__sync_bool_compare_and_swap(&top_level_lock2, tmp, tmp + 1)) {
+          break;
+        }
+      }
+      do {
+      } while(!__sync_bool_compare_and_swap(&top_level_lock2, kPhysAvailableCoreNum, kPhysAvailableCoreNum)); 
+     top_level_lock1 = 0;
+      while(true) {
+        int tmp = top_level_lock3;
+        if (__sync_bool_compare_and_swap(&top_level_lock3, tmp, tmp + 1)) {
+          break;
+        }
+      }
+      do {
+      } while(!__sync_bool_compare_and_swap(&top_level_lock3, kPhysAvailableCoreNum, kPhysAvailableCoreNum));
+    }
+    if (apicid % 4 == 0) {
+      second_level_lock2[apicid / 8].Init();
+      second_level_lock1[apicid / 8].Do();
+      second_level_lock3[apicid / 8].Init();
+      second_level_lock2[apicid / 8].Do();
+      second_level_lock1[apicid / 8].Init();
+      second_level_lock3[apicid / 8].Do();
+    }
+    third_level_lock2[apicid / 4].Init();
+    third_level_lock1[apicid / 4].Do();
+    third_level_lock3[apicid / 4].Init();
+    third_level_lock2[apicid / 4].Do();
+    third_level_lock1[apicid / 4].Init();
+    third_level_lock3[apicid / 4].Do();
+  }
+};
+
+struct Sync3 {
+  static const int kSubStructNum = 37;
+  int kPhysAvailableCoreNum;
+  int top_level_lock1;
+  int top_level_lock2;
+  int top_level_lock3;
+  struct SyncSub {
+    int lock;
+    void Init() {
+      lock = 0;
+    }
+    void Do() {
+      while(true) {
+        int tmp = lock;
+        if (__sync_bool_compare_and_swap(&lock, tmp, tmp + 1)) {
+          break;
+        }
+      }
+      do {
+      } while(!__sync_bool_compare_and_swap(&lock, 2, 2));
+    }
+  };
+  SyncSub second_level_lock1[kSubStructNum];
+  SyncSub second_level_lock2[kSubStructNum];
+  SyncSub second_level_lock3[kSubStructNum];
+  struct SyncSub2 {
+    int lock;
+    void Init() {
+      lock = 0;
+    }
+    void Do() {
+      while(true) {
+        int tmp = lock;
+        if (__sync_bool_compare_and_swap(&lock, tmp, tmp + 1)) {
+          break;
+        }
+      }
+      do {
+      } while(!__sync_bool_compare_and_swap(&lock, 4, 4));
+    }
+  };
+  SyncSub2 third_level_lock1[kSubStructNum*2];
+  SyncSub2 third_level_lock2[kSubStructNum*2];
+  SyncSub2 third_level_lock3[kSubStructNum*2];
+  void Init(int substruct_num) {
+    kPhysAvailableCoreNum = substruct_num;
     top_level_lock1 = 0;
     top_level_lock2 = 0;
     for (int i = 0; i < kSubStructNum; i++) {
@@ -745,6 +877,7 @@ static void membench4() {
   }
 }
 
+// syncのテスト
 static void membench5() {
   uint32_t ebx, edx, ecx;
   asm volatile("cpuid;":"=b"(ebx), "=d"(edx), "=c"(ecx):"a"(0));
@@ -800,6 +933,138 @@ static void membench5() {
 
 }
 
+// コアの個数を変えた同期
+static void membench6() {
+  uint32_t ebx, edx, ecx;
+  asm volatile("cpuid;":"=b"(ebx), "=d"(edx), "=c"(ecx):"a"(0));
+  char buf[12];
+  *(reinterpret_cast<uint32_t *>(buf + 0)) = ebx;
+  *(reinterpret_cast<uint32_t *>(buf + 4)) = edx;
+  *(reinterpret_cast<uint32_t *>(buf + 8)) = ecx;
+  if (strncmp(buf, "AuthenticAMD", 12) == 0) {
+    // QEMU
+    return;
+  }
+
+  int cpuid = cpu_ctrl->GetCpuId().GetRawId();
+  uint32_t apicid = cpu_ctrl->GetCpuId().GetApicId();
+
+  {
+    static volatile int l1 = 0, l2 = 0, l3 = 0;
+    sync(l1, l2, l3);
+  }
+
+  if (cpuid == 0) {
+    gtty->CprintfRaw("start >>>");
+  }
+  
+  for (int i = 1; i <= 37; i++) {
+    static volatile int cpu_num;
+    if (cpuid == 0) {
+      cpu_num = 0;
+    }
+    {
+      static volatile int l1 = 0, l2 = 0, l3 = 0;
+      sync(l1, l2, l3);
+    }
+    
+    if (apicid < i * 8) {
+      while(true) {
+	int tmp = cpu_num;
+	if (__sync_bool_compare_and_swap(&cpu_num, tmp, tmp + 1)) {
+	  break;
+	}
+      }
+    }
+
+    {
+      static volatile int l1 = 0, l2 = 0, l3 = 0;
+      sync(l1, l2, l3);
+    }
+
+    if (apicid < i * 8) {
+      {
+	static volatile int l1 = 0, l2 = 0, l3 = 0;
+	sync2(cpu_num, l1, l2, l3);
+      }
+  
+      uint64_t t1 = timer->ReadMainCnt();
+      for (int i = 0; i < 100; i++) {
+	static volatile int l1 = 0, l2 = 0, l3 = 0;
+	sync2(cpu_num, l1, l2, l3);
+      }
+      if (apicid == 0) {
+	gtty->CprintfRaw("<%d %d us> ", cpu_num, ((timer->ReadMainCnt() - t1) * timer->GetCntClkPeriod()) / 1000);
+	for (int j = 0; j < 256; j++) {
+	  CpuId cpuid(j);
+	  if (cpuid.GetApicId() != 0) {
+	    apic_ctrl->SendIpi(cpuid.GetApicId());
+	  }
+	}
+      }
+    } else {
+      asm volatile("hlt;");
+    }
+  }
+
+
+
+  for (int i = 1; i <= 37; i++) {
+    static volatile int cpu_num;
+    if (cpuid == 0) {
+      cpu_num = 0;
+    }
+    {
+      static volatile int l1 = 0, l2 = 0, l3 = 0;
+      sync(l1, l2, l3);
+    }
+    
+    if (apicid < i * 8) {
+      while(true) {
+	int tmp = cpu_num;
+	if (__sync_bool_compare_and_swap(&cpu_num, tmp, tmp + 1)) {
+	  break;
+	}
+      }
+    }
+
+    {
+      static volatile int l1 = 0, l2 = 0, l3 = 0;
+      sync(l1, l2, l3);
+    }
+
+    if (apicid < i * 8) {
+      assert(cpu_num % 8 == 0);
+      static Sync3 sync_;
+      sync_.Init(cpu_num / 8);
+
+      {
+	static volatile int l1 = 0, l2 = 0, l3 = 0;
+	sync2(cpu_num, l1, l2, l3);
+      }
+  
+      uint64_t t2 = timer->ReadMainCnt();
+      for (int i = 0; i < 100; i++) {
+	sync_.Do();
+      }
+
+      if (apicid == 0) {
+	//	gtty->CprintfRaw("<%d %d us> ", cpu_num, ((timer->ReadMainCnt() - t2) * timer->GetCntClkPeriod()) / 1000);
+	for (int j = 0; j < 256; j++) {
+	  CpuId cpuid(j);
+	  if (cpuid.GetApicId() != 0) {
+	    apic_ctrl->SendIpi(cpuid.GetApicId());
+	  }
+	}
+      }
+    } else {
+      asm volatile("hlt;");
+    }
+  }
+
+}
+
+
 Callout callout[256];
 static void register_membench_callout() {
   static int id = 0;
@@ -825,7 +1090,7 @@ static void register_membench2_callout() {
   new(&callout[cpuid]) Callout;
   Function oneshot_bench_func;
   oneshot_bench_func.Init([](void *){
-    membench5();
+    membench6();
   }, nullptr);
   callout[cpuid].Init(oneshot_bench_func);
   callout[cpuid].SetHandler(10);
