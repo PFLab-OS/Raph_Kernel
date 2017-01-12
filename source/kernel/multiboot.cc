1ea0d4c44126c5bd85b61d3c1ab5fca3e5ab7b5b
liva <sap.pcmail@gmail.com>
2017-01-13 04:47:34 +0900

    move function definition to source file from header file

--- source/kernel/multiboot.cc
+++ source/kernel/multiboot.cc
@@ -91,3 +91,55 @@ void MultibootCtrl::Setup() {
     }
   }
 }
+
+void MultibootCtrl::ShowModuleInfo() {
+  kassert(align(multiboot_info, 8) == multiboot_info);
+  virt_addr addr = p2v(static_cast<phys_addr>(multiboot_info));
+  addr += 8;
+  gtty->CprintfRaw("\n");
+  for (multiboot_tag *tag = reinterpret_cast<multiboot_tag *>(addr); tag->type != MULTIBOOT_TAG_TYPE_END; addr = alignUp(addr + tag->size, 8), tag = reinterpret_cast<multiboot_tag *>(addr)) {
+    switch(tag->type) {
+    case MULTIBOOT_TAG_TYPE_MODULE: {
+      multiboot_tag_module* info = (struct multiboot_tag_module *) tag;
+      gtty->CprintfRaw("module: %x %x\n", info->mod_start, info->mod_end);
+      break;
+    }
+    default:
+      break;
+    }
+  }
+}
+
+void MultibootCtrl::ShowBuildTimeStamp() {
+  virt_addr addr = p2v(static_cast<phys_addr>(multiboot_info));
+  addr += 8;
+  multiboot_tag_module* info = nullptr;
+  for (multiboot_tag *tag = reinterpret_cast<multiboot_tag *>(addr); tag->type != MULTIBOOT_TAG_TYPE_END; addr = alignUp(addr + tag->size, 8), tag = reinterpret_cast<multiboot_tag *>(addr)) {
+    bool break_flag = false;
+    switch(tag->type) {
+    case MULTIBOOT_TAG_TYPE_MODULE: {
+      info = (struct multiboot_tag_module *) tag;
+      if (strcmp(info->cmdline, "time") == 0) {
+        break_flag = true;
+      }
+      break;
+    }
+    default:
+      break;
+    }
+    if (break_flag) {
+      break;
+    }
+  }
+  size_t len = info->mod_end - info->mod_start;
+  char str[len + 1];
+  for (size_t i = 0; i < len; i++) {
+    str[i] = reinterpret_cast<uint8_t *>(info->mod_start)[i];
+  }
+  str[len] = '\0';
+  if (info == nullptr) {
+    gtty->Cprintf("[kernel] warning: No build information\n");
+  } else {
+    gtty->Cprintf("[kernel] info: Build Information as follows\n%s\n", str);
+  }
+}
