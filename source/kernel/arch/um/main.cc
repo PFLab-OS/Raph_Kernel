5a5adfad0997b64fdea69c9210cc3a70b2c27dce
liva <sap.pcmail@gmail.com>
2017-07-28 11:44:50 +0900

    add stub of arch um

---
+++ source/kernel/arch/um/main.cc
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int rval = -1;
+  printf("=== user mode Raph_Kernel(experimental)\t===\n");
+  printf("===         main() returned %d        \t===\n", rval);
+  return rval;
+}
