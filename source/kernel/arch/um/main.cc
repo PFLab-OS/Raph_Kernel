911cf92c96201b5acaf6b23fdc5c18d5e870f803
liva <sap.pcmail@gmail.com>
2017-07-30 16:08:50 +0900

    add include options to arch 'um'

--- source/kernel/arch/um/main.cc
+++ source/kernel/arch/um/main.cc
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int rval = -1;
+  int rval = 0;
   printf("=== user mode Raph_Kernel(experimental)\t===\n");
   printf("===         main() returned %d        \t===\n", rval);
   return rval;
