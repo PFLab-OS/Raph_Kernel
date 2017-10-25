1a952a0ab7d883ea5bd0ab6d0da7d472fa060f24
hikalium <hikalium@hikalium.com>
2017-10-25 23:39:58 +0900

    Fix framebuffer (but not sure why that bug happend)

--- source/kernel/dev/framebuffer.cc
+++ source/kernel/dev/framebuffer.cc
@@ -14,12 +14,13 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
  * Author: Liva
  *
  * graphic driver for framebuffer
- * 
+ *
  */
 
 #include <global.h>
@@ -54,10 +55,11 @@ void FrameBuffer::Init() {
   }
 
   _font_buffer = new bool[_font_normal.GetMaxh() * _font_normal.GetMaxw()];
-  _info_row = (_fb_info.height / _font_normal.GetMaxh() - 1) * _font_normal.GetMaxh();
+  _info_row =
+      (_fb_info.height / _font_normal.GetMaxh() - 1) * _font_normal.GetMaxh();
 
   _info_buffer = new StringBuffer(&_fb_info, &_font_normal);
-  
+
   _draw_cpuid = cpu_ctrl->RetainCpuIdForPurpose(CpuPurpose::kLowPriority);
 }
 
@@ -74,7 +76,7 @@ void FrameBuffer::WriteErr(char c) {
     _cey += maxh;
     return;
   }
-    
+
   int width;
   _font_normal.GetData(c, width, _font_buffer);
   if (_cex + width >= _fb_info.width) {
@@ -86,7 +88,9 @@ void FrameBuffer::WriteErr(char c) {
   }
   for (int y = 0; y < maxh; y++) {
     for (int x = 0; x < width; x++) {
-      uint8_t *buf = _fb_info.buffer + (_cex + x + (_cey + y) * _fb_info.width) * (_fb_info.bpp / 8);
+      uint8_t *buf =
+          _fb_info.buffer +
+          (_cex + x + (_cey + y) * _fb_info.width) * (_fb_info.bpp / 8);
       if (_font_buffer[y * width + x]) {
         buf[0] = 0xff;
         buf[1] = 0xff;
@@ -98,13 +102,13 @@ void FrameBuffer::WriteErr(char c) {
       }
     }
   }
-  _cex+=width;
+  _cex += width;
 }
 
 void FrameBuffer::PrintShell(const char *str) {
   Locker locker(_lock);
   if (_shell_buffer_len < strlen(str) + 1) {
-    delete [] _shell_buffer;
+    delete[] _shell_buffer;
     _shell_buffer = new char[(strlen(str) + 1) * 2];
   }
   strcpy(_shell_buffer, str);
@@ -122,9 +126,10 @@ void FrameBuffer::DoPeriodicRefresh(void *) {
 void FrameBuffer::DisableTimeupDraw() {
   _timeup_draw = false;
   _needsRedraw = true;
-    
+
   _refresh_callout = make_sptr(new Callout);
-  _refresh_callout->Init(make_uptr(new ClassFunction<FrameBuffer, void *>(this, &FrameBuffer::DoPeriodicRefresh, nullptr)));
+  _refresh_callout->Init(make_uptr(new ClassFunction<FrameBuffer, void *>(
+      this, &FrameBuffer::DoPeriodicRefresh, nullptr)));
   ScheduleRefresh();
 }
 
@@ -133,26 +138,33 @@ void FrameBuffer::DrawScreen() {
   if (_disable_flag) return;
   //
   assert(_lock.IsLocked());
-  // draw prompt 
-  int xoffset = _font_inverted.PrintChar(_fb_info.buffer, _fb_info.width,
-                                         0, _fb_info.height - _font_inverted.GetMaxh(), U'>');
+  // draw prompt
+  int xoffset = _font_inverted.PrintChar(
+      _fb_info.buffer, _fb_info.width, 0,
+      _fb_info.height - _font_inverted.GetMaxh(), U'>');
 
   if (_shell_buffer) {
-    // TODO consider overflow 
+    // TODO consider overflow
     for (int x = 0; x < strlen(_shell_buffer); x++) {
       if (_shell_buffer[x] == '\0') {
         return;
       }
-      int width = _font_inverted.GetWidth(static_cast<char32_t>(_shell_buffer[x]));
+      int width =
+          _font_inverted.GetWidth(static_cast<char32_t>(_shell_buffer[x]));
       if (xoffset + width > _fb_info.width) {
         break;
       }
-      xoffset += _font_inverted.PrintChar(_fb_info.buffer, _fb_info.width,
-                                          xoffset, _fb_info.height - _font_inverted.GetMaxh(), _shell_buffer[x]); ;
+      xoffset += _font_inverted.PrintChar(
+          _fb_info.buffer, _fb_info.width, xoffset,
+          _fb_info.height - _font_inverted.GetMaxh(), _shell_buffer[x]);
+      ;
     }
   }
   for (int y = 0; y < _font_inverted.GetMaxh(); y++) {
-    memset(_fb_info.buffer + (xoffset + (_fb_info.height - y - 1) * _fb_info.width) * (_fb_info.bpp / 8), 0xff, (_fb_info.width - xoffset) * (_fb_info.bpp / 8));
+    memset(_fb_info.buffer +
+               (xoffset + (_fb_info.height - y - 1) * _fb_info.width) *
+                   (_fb_info.bpp / 8),
+           0xff, (_fb_info.width - xoffset) * (_fb_info.bpp / 8));
   }
 
   DrawScreenSub();
@@ -160,10 +172,10 @@ void FrameBuffer::DrawScreen() {
 
 void FrameBuffer::DrawScreenSub() {
   int row = 0;
-  // count sum 
+  // count sum
   do {
     StringBuffer *buf = _info_buffer;
-    while(true) {
+    while (true) {
       row += buf->_row * _font_normal.GetMaxh();
       if (buf->_next == nullptr) {
         if (buf->_buffer[buf->_length - 1] == StringBuffer::kNewLineSignature) {
@@ -173,19 +185,22 @@ void FrameBuffer::DrawScreenSub() {
       }
       buf = buf->_next;
     }
-  } while(0);
+  } while (0);
 
-  while(row - _info_buffer->_row * _font_normal.GetMaxh() >= _info_row) {
+  while (row - _info_buffer->_row * _font_normal.GetMaxh() >= _info_row) {
     StringBuffer *buf = _info_buffer;
     _info_buffer = buf->_next;
     buf->_next = nullptr;
     row -= buf->_row * _font_normal.GetMaxh();
     delete buf;
   }
-   
+  // OK
+
+  // this??????????
+
   StringBuffer *buf = _info_buffer;
   int index = 0;
-  while(row >= _info_row) {
+  while (row >= _info_row) {
     for (; index < buf->_length; index++) {
       if (buf->_buffer[index] == StringBuffer::kNewLineSignature) {
         row -= _font_normal.GetMaxh();
@@ -193,37 +208,38 @@ void FrameBuffer::DrawScreenSub() {
         break;
       }
     }
-    if (index == buf->_length) {
-      buf = buf->_next;
-      index = 0;
-    }
-    assert(buf != nullptr);
   }
   int y = 0;
   int x = 0;
-  while(buf != nullptr) {
-    for(; index < buf->_length; index++) {
+  int bytes_per_pixel = _fb_info.bpp / 8;
+  while (buf != nullptr) {
+    for (; index < buf->_length; index++) {
       uint32_t i = buf->_buffer[index];
       char32_t c = buf->_cBuffer[index];
       if (i == StringBuffer::kNewLineSignature) {
-        int ny = y + _font_normal.GetMaxh();
-        for (; y < ny; y++) {
-          bzero(_fb_info.buffer + (x + y * _fb_info.width) * (_fb_info.bpp / 8),
-                (_fb_info.width - x) * (_fb_info.bpp / 8));
+        for (int py = y; py < y + _font_normal.GetMaxh(); py++) {
+          // assert(x >= 0 && x < _fb_info.width);
+          for (int px = x; px < _fb_info.width; px++) {
+            for (int p = 0; p < bytes_per_pixel; p++) {
+              _fb_info
+                  .buffer[(py * _fb_info.width + px) * bytes_per_pixel + p] = 0;
+            }
+          }
         }
+        y += _font_normal.GetMaxh();
         x = 0;
       } else {
         x += _font_normal.PrintChar(_fb_info.buffer, _fb_info.width, x, y, c);
       }
     }
-      
     buf = buf->_next;
     index = 0;
   }
   if (y < _info_row) {
     int ny = y + _font_normal.GetMaxh();
     for (; y < ny; y++) {
-      bzero(_fb_info.buffer + (x + y * _fb_info.width) * (_fb_info.bpp / 8), (_fb_info.width - x) * (_fb_info.bpp / 8));
+      bzero(_fb_info.buffer + (x + y * _fb_info.width) * (_fb_info.bpp / 8),
+            (_fb_info.width - x) * (_fb_info.bpp / 8));
     }
   } else if (y == _info_row) {
     assert(x == 0);
