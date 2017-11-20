0374c2e2168685a1ca6c243f4c0f15afc3634fb1
hikalium <hikalium@hikalium.com>
2017-11-20 13:40:03 +0900

    Remove all VirtmemCtrl::New and Delete (deprecated)

--- source/kernel/acpica/os_specific/service_layers/osraph.cc
+++ source/kernel/acpica/os_specific/service_layers/osraph.cc
@@ -14,10 +14,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
  *
  * Author: Liva
- * 
+ *
  */
 
 #include <raph.h>
@@ -39,242 +40,247 @@ extern "C" {
 #include "acparser.h"
 #include "acdebug.h"
 
-  struct AcpiIntHandler {
-    ACPI_OSD_HANDLER func;
-    int vector;
-  };
-  static AcpiIntHandler handler[10];
-  ACPI_STATUS AcpiOsInitialize() {
-    for (int i = 0; i < 10; i++) {
-      handler[i].func = nullptr;
-    }
-    return (AE_OK);
+struct AcpiIntHandler {
+  ACPI_OSD_HANDLER func;
+  int vector;
+};
+static AcpiIntHandler handler[10];
+ACPI_STATUS AcpiOsInitialize() {
+  for (int i = 0; i < 10; i++) {
+    handler[i].func = nullptr;
   }
-  
-  ACPI_STATUS AcpiOsTerminate() {
-    return (AE_OK);
-  }
+  return (AE_OK);
+}
 
-  ACPI_PHYSICAL_ADDRESS AcpiOsGetRootPointer() {
-    phys_addr ptr = multiboot_ctrl->GetAcpiRoot();
-    if (ptr == 0) {
-      kernel_panic("ACPICA", "no RSDP");
-    }
-    return ptr;
-  }
+ACPI_STATUS AcpiOsTerminate() { return (AE_OK); }
 
-  ACPI_STATUS AcpiOsPredefinedOverride(const ACPI_PREDEFINED_NAMES *PredefinedObject, ACPI_STRING *NewValue) {
-    *NewValue = NULL; 
-    return (AE_OK);
+ACPI_PHYSICAL_ADDRESS AcpiOsGetRootPointer() {
+  phys_addr ptr = multiboot_ctrl->GetAcpiRoot();
+  if (ptr == 0) {
+    kernel_panic("ACPICA", "no RSDP");
   }
+  return ptr;
+}
 
-  ACPI_STATUS AcpiOsTableOverride(ACPI_TABLE_HEADER *ExistingTable, ACPI_TABLE_HEADER **NewTable) {
-    *NewTable = NULL;
-    return (AE_NO_ACPI_TABLES);
-  }
+ACPI_STATUS AcpiOsPredefinedOverride(
+    const ACPI_PREDEFINED_NAMES *PredefinedObject, ACPI_STRING *NewValue) {
+  *NewValue = NULL;
+  return (AE_OK);
+}
 
-  ACPI_STATUS AcpiOsPhysicalTableOverride(ACPI_TABLE_HEADER *ExistingTable, ACPI_PHYSICAL_ADDRESS *NewAddress, UINT32 *NewTableLength) {
-    return (AE_SUPPORT);
-  }
+ACPI_STATUS AcpiOsTableOverride(ACPI_TABLE_HEADER *ExistingTable,
+                                ACPI_TABLE_HEADER **NewTable) {
+  *NewTable = NULL;
+  return (AE_NO_ACPI_TABLES);
+}
 
-  void *AcpiOsMapMemory(ACPI_PHYSICAL_ADDRESS PhysicalAddress, ACPI_SIZE Length) {
-    return reinterpret_cast<void *>(p2v(PhysicalAddress));
-  }
+ACPI_STATUS AcpiOsPhysicalTableOverride(ACPI_TABLE_HEADER *ExistingTable,
+                                        ACPI_PHYSICAL_ADDRESS *NewAddress,
+                                        UINT32 *NewTableLength) {
+  return (AE_SUPPORT);
+}
 
-  void AcpiOsUnmapMemory(void *where, ACPI_SIZE length) {
-  }
+void *AcpiOsMapMemory(ACPI_PHYSICAL_ADDRESS PhysicalAddress, ACPI_SIZE Length) {
+  return reinterpret_cast<void *>(p2v(PhysicalAddress));
+}
 
-  ACPI_STATUS AcpiOsGetPhysicalAddress(void *LogicalAddress, ACPI_PHYSICAL_ADDRESS *PhysicalAddress) {
-    *PhysicalAddress = v2p(reinterpret_cast<virt_addr>(LogicalAddress));
-    return (AE_OK);
-  }
+void AcpiOsUnmapMemory(void *where, ACPI_SIZE length) {}
 
-  void *AcpiOsAllocate(ACPI_SIZE Size) {
-    return reinterpret_cast<void *>(virtmem_ctrl->Alloc(Size));
-  }
+ACPI_STATUS AcpiOsGetPhysicalAddress(void *LogicalAddress,
+                                     ACPI_PHYSICAL_ADDRESS *PhysicalAddress) {
+  *PhysicalAddress = v2p(reinterpret_cast<virt_addr>(LogicalAddress));
+  return (AE_OK);
+}
 
-  void AcpiOsFree(void *Memory) {
-    virtmem_ctrl->Free(reinterpret_cast<virt_addr>(Memory));
-  }
+void *AcpiOsAllocate(ACPI_SIZE Size) {
+  return reinterpret_cast<void *>(virtmem_ctrl->Alloc(Size));
+}
 
-  BOOLEAN AcpiOsReadable(void *Memory, ACPI_SIZE Length) {
-    kassert(false);
-    return false;
-  }
+void AcpiOsFree(void *Memory) {
+  virtmem_ctrl->Free(reinterpret_cast<virt_addr>(Memory));
+}
 
-  BOOLEAN AcpiOsWritable(void *Memory, ACPI_SIZE Length) {
-    kassert(false);
-    return false;
-  }
-  
-  ACPI_THREAD_ID AcpiOsGetThreadId() {
-    return 1;
-  }
+BOOLEAN AcpiOsReadable(void *Memory, ACPI_SIZE Length) {
+  kassert(false);
+  return false;
+}
 
-  ACPI_STATUS AcpiOsExecute(ACPI_EXECUTE_TYPE Type, ACPI_OSD_EXEC_CALLBACK Function, void *Context) {
-    kassert(false);
-    return (AE_ERROR);
-  }
+BOOLEAN AcpiOsWritable(void *Memory, ACPI_SIZE Length) {
+  kassert(false);
+  return false;
+}
 
-  ACPI_STATUS AcpiOsSignal(UINT32 Function, void *Info) {
-    kassert(false);
-    return (AE_ERROR);
-  }
+ACPI_THREAD_ID AcpiOsGetThreadId() { return 1; }
 
-  void AcpiOsWaitEventsComplete() {
-    kassert(false);
-  }
+ACPI_STATUS AcpiOsExecute(ACPI_EXECUTE_TYPE Type,
+                          ACPI_OSD_EXEC_CALLBACK Function, void *Context) {
+  kassert(false);
+  return (AE_ERROR);
+}
 
-  UINT64 AcpiOsGetTimer() {
-    // fix this
-    return timer->GetUsecFromCnt(timer->ReadMainCnt()) * 10;
-  }
+ACPI_STATUS AcpiOsSignal(UINT32 Function, void *Info) {
+  kassert(false);
+  return (AE_ERROR);
+}
 
-  void AcpiOsSleep(UINT64 Milliseconds) {
-    timer->BusyUwait(Milliseconds * 1000);
-  }
+void AcpiOsWaitEventsComplete() { kassert(false); }
 
-  void AcpiOsStall(UINT32 Microseconds) {
-    timer->BusyUwait(Microseconds);
-  }
+UINT64 AcpiOsGetTimer() {
+  // fix this
+  return timer->GetUsecFromCnt(timer->ReadMainCnt()) * 10;
+}
 
-  ACPI_STATUS AcpiOsCreateMutex(ACPI_MUTEX *OutHandle) {
-    SpinLock *lock = virtmem_ctrl->New<SpinLock>();
-    *OutHandle = reinterpret_cast<ACPI_MUTEX>(lock);
-    return (AE_OK);
-  }
+void AcpiOsSleep(UINT64 Milliseconds) { timer->BusyUwait(Milliseconds * 1000); }
 
-  void AcpiOsDeleteMutex(ACPI_MUTEX Handle) {
-    SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
-    virtmem_ctrl->Delete(lock);
-  }
-  
-  ACPI_STATUS AcpiOsAcquireMutex(ACPI_MUTEX Handle, UINT16 Timeout) {
-    SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
-    if (Timeout == 0) {
-      return (lock->Trylock() == ReturnState::kOk) ? (AE_OK) : (AE_ERROR);
-    } else if (Timeout > 0) {
-      uint64_t cnt = timer->GetCntAfterPeriod(timer->ReadMainCnt(), Timeout * 1000);
-      while(!timer->IsTimePassed(cnt)) {
-        if (lock->Trylock() == ReturnState::kOk) {
-          return (AE_OK);
-        }
+void AcpiOsStall(UINT32 Microseconds) { timer->BusyUwait(Microseconds); }
+
+ACPI_STATUS AcpiOsCreateMutex(ACPI_MUTEX *OutHandle) {
+  SpinLock *lock = new SpinLock();
+  *OutHandle = reinterpret_cast<ACPI_MUTEX>(lock);
+  return (AE_OK);
+}
+
+void AcpiOsDeleteMutex(ACPI_MUTEX Handle) {
+  SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+  delete lock;
+}
+
+ACPI_STATUS AcpiOsAcquireMutex(ACPI_MUTEX Handle, UINT16 Timeout) {
+  SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+  if (Timeout == 0) {
+    return (lock->Trylock() == ReturnState::kOk) ? (AE_OK) : (AE_ERROR);
+  } else if (Timeout > 0) {
+    uint64_t cnt =
+        timer->GetCntAfterPeriod(timer->ReadMainCnt(), Timeout * 1000);
+    while (!timer->IsTimePassed(cnt)) {
+      if (lock->Trylock() == ReturnState::kOk) {
+        return (AE_OK);
       }
-      return (AE_ERROR);
-    } else {
-      lock->Lock();
-      return (AE_OK);
     }
-  } 
-
-  void AcpiOsReleaseMutex(ACPI_MUTEX Handle) {
-    SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
-    lock->Unlock();
-  }
-
-  ACPI_STATUS AcpiOsCreateSemaphore(UINT32 MaxUnits, UINT32 InitialUnits, ACPI_SEMAPHORE *OutHandle) {
-    Semaphore *semaphore = new Semaphore(MaxUnits, InitialUnits);
-    *OutHandle = reinterpret_cast<ACPI_SEMAPHORE>(semaphore);
-    return (AE_OK);
-  }
-
-  ACPI_STATUS AcpiOsDeleteSemaphore(ACPI_SEMAPHORE Handle) {
-    Semaphore *semaphore = reinterpret_cast<Semaphore *>(Handle);
-    virtmem_ctrl->Delete(semaphore);
-    return (AE_OK);
-  }
-
-  ACPI_STATUS AcpiOsWaitSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units, UINT16 Timeout) {
-    Semaphore *semaphore = reinterpret_cast<Semaphore *>(Handle);
-    if (Timeout == 0) {
-      return (semaphore->Tryacquire() == 0) ? (AE_OK) : (AE_ERROR);
-    } else if (Timeout > 0) {
-      uint64_t cnt = timer->GetCntAfterPeriod(timer->ReadMainCnt(), Timeout * 1000);
-      while(!timer->IsTimePassed(cnt)) {
-        if (semaphore->Tryacquire() == 0) {
-          return (AE_OK);
-        }
-      }
-      return (AE_ERROR);
-    } else {
-      semaphore->Acquire();
-      return (AE_OK);
-    }
-  }
-
-  ACPI_STATUS AcpiOsSignalSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units) {
-    Semaphore *semaphore = reinterpret_cast<Semaphore *>(Handle);
-    for(; Units > 0; Units--) {
-      semaphore->Release();
-    }
-    return (AE_OK);
-  }
-
-  ACPI_STATUS AcpiOsCreateLock(ACPI_SPINLOCK *OutHandle) {
-    SpinLock *lock = virtmem_ctrl->New<SpinLock>();
-    *OutHandle = reinterpret_cast<ACPI_SPINLOCK>(lock);
-    return (AE_OK);
-  }
-
-  void AcpiOsDeleteLock(ACPI_HANDLE Handle) {
-    SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
-    virtmem_ctrl->Delete(lock);
-  }
-
-  ACPI_CPU_FLAGS AcpiOsAcquireLock(ACPI_SPINLOCK Handle) {
-    SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+    return (AE_ERROR);
+  } else {
     lock->Lock();
-    return 0;
-  }
-
-  void AcpiOsReleaseLock(ACPI_SPINLOCK Handle, ACPI_CPU_FLAGS Flags) {
-    SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
-    lock->Unlock();
-  }
-
-  static void AcpiHandlerSub(Regs *rs, void *arg) {
-    for(int i = 0; i < 10; i++) {
-      if (static_cast<int>(rs->n) == handler[i].vector) {
-        handler[i].func(arg);
-        return;
-      }
-    }
-  }
-
-  ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 InterruptLevel, ACPI_OSD_HANDLER Handler, void *Context) {
-    kassert(apic_ctrl != nullptr);
-    kassert(idt != nullptr);
-    CpuId cpuid = cpu_ctrl->RetainCpuIdForPurpose(CpuPurpose::kLowPriority);
-    int vector = idt->SetIntCallback(cpuid, AcpiHandlerSub, Context, Idt::EoiType::kLapic);
-    for(int i = 0; i < 10; i++) {
-      if (handler[i].func == nullptr) {
-        handler[i].func = Handler;
-        handler[i].vector = vector;
-      }
-    }
-    apic_ctrl->SetupIoInt(InterruptLevel, apic_ctrl->GetApicIdFromCpuId(cpuid), vector, true, true);
     return (AE_OK);
   }
+}
 
-  ACPI_STATUS AcpiOsRemoveInterruptHandler(UINT32 InterruptNumber, ACPI_OSD_HANDLER Handler) {
-    // TODO fix this
+void AcpiOsReleaseMutex(ACPI_MUTEX Handle) {
+  SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+  lock->Unlock();
+}
+
+ACPI_STATUS AcpiOsCreateSemaphore(UINT32 MaxUnits, UINT32 InitialUnits,
+                                  ACPI_SEMAPHORE *OutHandle) {
+  Semaphore *semaphore = new Semaphore(MaxUnits, InitialUnits);
+  *OutHandle = reinterpret_cast<ACPI_SEMAPHORE>(semaphore);
+  return (AE_OK);
+}
+
+ACPI_STATUS AcpiOsDeleteSemaphore(ACPI_SEMAPHORE Handle) {
+  Semaphore *semaphore = reinterpret_cast<Semaphore *>(Handle);
+  delete semaphore;
+  return (AE_OK);
+}
+
+ACPI_STATUS AcpiOsWaitSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units,
+                                UINT16 Timeout) {
+  Semaphore *semaphore = reinterpret_cast<Semaphore *>(Handle);
+  if (Timeout == 0) {
+    return (semaphore->Tryacquire() == 0) ? (AE_OK) : (AE_ERROR);
+  } else if (Timeout > 0) {
+    uint64_t cnt =
+        timer->GetCntAfterPeriod(timer->ReadMainCnt(), Timeout * 1000);
+    while (!timer->IsTimePassed(cnt)) {
+      if (semaphore->Tryacquire() == 0) {
+        return (AE_OK);
+      }
+    }
+    return (AE_ERROR);
+  } else {
+    semaphore->Acquire();
     return (AE_OK);
   }
-  
-  void ACPI_INTERNAL_VAR_XFACE AcpiOsPrintf(const char *Fmt, ...) {
-    va_list Args;
-    va_start(Args, Fmt);
-    AcpiOsVprintf(Fmt, Args);
-    va_end(Args);
-  }
+}
 
-  void AcpiOsVprintf(const char *Fmt, va_list Args) {
-    kassert(gtty != nullptr);
-    gtty->Vprintf(Fmt, Args);
+ACPI_STATUS AcpiOsSignalSemaphore(ACPI_SEMAPHORE Handle, UINT32 Units) {
+  Semaphore *semaphore = reinterpret_cast<Semaphore *>(Handle);
+  for (; Units > 0; Units--) {
+    semaphore->Release();
   }
+  return (AE_OK);
+}
 
-  ACPI_STATUS AcpiOsReadMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 *Value, UINT32 Width) {
-    switch(Width) {
+ACPI_STATUS AcpiOsCreateLock(ACPI_SPINLOCK *OutHandle) {
+  SpinLock *lock = new SpinLock();
+  *OutHandle = reinterpret_cast<ACPI_SPINLOCK>(lock);
+  return (AE_OK);
+}
+
+void AcpiOsDeleteLock(ACPI_HANDLE Handle) {
+  SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+  delete lock;
+}
+
+ACPI_CPU_FLAGS AcpiOsAcquireLock(ACPI_SPINLOCK Handle) {
+  SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+  lock->Lock();
+  return 0;
+}
+
+void AcpiOsReleaseLock(ACPI_SPINLOCK Handle, ACPI_CPU_FLAGS Flags) {
+  SpinLock *lock = reinterpret_cast<SpinLock *>(Handle);
+  lock->Unlock();
+}
+
+static void AcpiHandlerSub(Regs *rs, void *arg) {
+  for (int i = 0; i < 10; i++) {
+    if (static_cast<int>(rs->n) == handler[i].vector) {
+      handler[i].func(arg);
+      return;
+    }
+  }
+}
+
+ACPI_STATUS AcpiOsInstallInterruptHandler(UINT32 InterruptLevel,
+                                          ACPI_OSD_HANDLER Handler,
+                                          void *Context) {
+  kassert(apic_ctrl != nullptr);
+  kassert(idt != nullptr);
+  CpuId cpuid = cpu_ctrl->RetainCpuIdForPurpose(CpuPurpose::kLowPriority);
+  int vector =
+      idt->SetIntCallback(cpuid, AcpiHandlerSub, Context, Idt::EoiType::kLapic);
+  for (int i = 0; i < 10; i++) {
+    if (handler[i].func == nullptr) {
+      handler[i].func = Handler;
+      handler[i].vector = vector;
+    }
+  }
+  apic_ctrl->SetupIoInt(InterruptLevel, apic_ctrl->GetApicIdFromCpuId(cpuid),
+                        vector, true, true);
+  return (AE_OK);
+}
+
+ACPI_STATUS AcpiOsRemoveInterruptHandler(UINT32 InterruptNumber,
+                                         ACPI_OSD_HANDLER Handler) {
+  // TODO fix this
+  return (AE_OK);
+}
+
+void ACPI_INTERNAL_VAR_XFACE AcpiOsPrintf(const char *Fmt, ...) {
+  va_list Args;
+  va_start(Args, Fmt);
+  AcpiOsVprintf(Fmt, Args);
+  va_end(Args);
+}
+
+void AcpiOsVprintf(const char *Fmt, va_list Args) {
+  kassert(gtty != nullptr);
+  gtty->Vprintf(Fmt, Args);
+}
+
+ACPI_STATUS AcpiOsReadMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 *Value,
+                             UINT32 Width) {
+  switch (Width) {
     case 8: {
       *Value = *(reinterpret_cast<uint8_t *>(p2v(Address)));
       break;
@@ -295,12 +301,13 @@ extern "C" {
       kassert(false);
       return (AE_ERROR);
     }
-    }
-    return (AE_OK);
   }
+  return (AE_OK);
+}
 
-  ACPI_STATUS AcpiOsWriteMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 Value, UINT32 Width) {
-    switch(Width) {
+ACPI_STATUS AcpiOsWriteMemory(ACPI_PHYSICAL_ADDRESS Address, UINT64 Value,
+                              UINT32 Width) {
+  switch (Width) {
     case 8: {
       *(reinterpret_cast<uint8_t *>(p2v(Address))) = Value;
       break;
@@ -321,12 +328,13 @@ extern "C" {
       kassert(false);
       return (AE_ERROR);
     }
-    }
-    return (AE_OK);
   }
+  return (AE_OK);
+}
 
-  ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS Address, UINT32 *Value, UINT32 Width) {
-    switch(Width) {
+ACPI_STATUS AcpiOsReadPort(ACPI_IO_ADDRESS Address, UINT32 *Value,
+                           UINT32 Width) {
+  switch (Width) {
     case 8: {
       *Value = inb(Address);
       break;
@@ -342,12 +350,13 @@ extern "C" {
     default: {
       kassert(false);
       return (AE_ERROR);
-    } 
     }
-    return (AE_OK);
   }
-  ACPI_STATUS AcpiOsWritePort(ACPI_IO_ADDRESS Address, UINT32 Value, UINT32 Width) {
-    switch(Width) {
+  return (AE_OK);
+}
+ACPI_STATUS AcpiOsWritePort(ACPI_IO_ADDRESS Address, UINT32 Value,
+                            UINT32 Width) {
+  switch (Width) {
     case 8: {
       outb(Address, Value);
       break;
@@ -363,56 +372,63 @@ extern "C" {
     default: {
       kassert(false);
       return (AE_ERROR);
-    } 
     }
-    return (AE_OK);
   }
-  
-  ACPI_STATUS AcpiOsReadPciConfiguration (ACPI_PCI_ID *PciId, UINT32 Register, UINT64 *Value, UINT32 Width) {
-    kassert(pci_ctrl != nullptr);
-    switch(Width) {
+  return (AE_OK);
+}
+
+ACPI_STATUS AcpiOsReadPciConfiguration(ACPI_PCI_ID *PciId, UINT32 Register,
+                                       UINT64 *Value, UINT32 Width) {
+  kassert(pci_ctrl != nullptr);
+  switch (Width) {
     case 8: {
-      *(reinterpret_cast<uint8_t *>(Value)) = pci_ctrl->ReadPciReg<uint8_t>(PciId->Bus, PciId->Device, PciId->Function, Register);
+      *(reinterpret_cast<uint8_t *>(Value)) = pci_ctrl->ReadPciReg<uint8_t>(
+          PciId->Bus, PciId->Device, PciId->Function, Register);
       break;
     }
     case 16: {
-      *(reinterpret_cast<uint16_t *>(Value)) = pci_ctrl->ReadPciReg<uint16_t>(PciId->Bus, PciId->Device, PciId->Function, Register);
+      *(reinterpret_cast<uint16_t *>(Value)) = pci_ctrl->ReadPciReg<uint16_t>(
+          PciId->Bus, PciId->Device, PciId->Function, Register);
       break;
     }
     case 32: {
-      *(reinterpret_cast<uint32_t *>(Value)) = pci_ctrl->ReadPciReg<uint32_t>(PciId->Bus, PciId->Device, PciId->Function, Register);
+      *(reinterpret_cast<uint32_t *>(Value)) = pci_ctrl->ReadPciReg<uint32_t>(
+          PciId->Bus, PciId->Device, PciId->Function, Register);
       break;
     }
     default: {
       kassert(false);
       return (AE_ERROR);
-    } 
     }
-    return (AE_OK);
   }
+  return (AE_OK);
+}
 
-  ACPI_STATUS AcpiOsWritePciConfiguration (ACPI_PCI_ID *PciId, UINT32 Register, ACPI_INTEGER Value, UINT32 Width) {
-    kassert(pci_ctrl != nullptr);
-    switch(Width) {
+ACPI_STATUS AcpiOsWritePciConfiguration(ACPI_PCI_ID *PciId, UINT32 Register,
+                                        ACPI_INTEGER Value, UINT32 Width) {
+  kassert(pci_ctrl != nullptr);
+  switch (Width) {
     case 8: {
-      pci_ctrl->WritePciReg<uint8_t>(PciId->Bus, PciId->Device, PciId->Function, Register, Value);
+      pci_ctrl->WritePciReg<uint8_t>(PciId->Bus, PciId->Device, PciId->Function,
+                                     Register, Value);
       break;
     }
     case 16: {
-      pci_ctrl->WritePciReg<uint16_t>(PciId->Bus, PciId->Device, PciId->Function, Register, Value);
+      pci_ctrl->WritePciReg<uint16_t>(PciId->Bus, PciId->Device,
+                                      PciId->Function, Register, Value);
       break;
     }
     case 32: {
-      pci_ctrl->WritePciReg<uint32_t>(PciId->Bus, PciId->Device, PciId->Function, Register, Value);
+      pci_ctrl->WritePciReg<uint32_t>(PciId->Bus, PciId->Device,
+                                      PciId->Function, Register, Value);
       break;
     }
     default: {
       kassert(false);
       return (AE_ERROR);
-    } 
     }
-    return (AE_OK);
   }
-
+  return (AE_OK);
+}
 }
 
