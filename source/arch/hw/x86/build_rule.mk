ccfc815546c97b983181a3a639b33724bc699e3e
liva <sap.pcmail@gmail.com>
2017-11-25 17:35:03 +0900

    fix corner case of init_script

--- source/arch/hw/x86/build_rule.mk
+++ source/arch/hw/x86/build_rule.mk
@@ -14,7 +14,7 @@ ifdef INIT
 	INIT_FILE = $(INIT)
 else
 	BRANCH_INIT_FILE = $(shell git rev-parse --abbrev-ref HEAD)
-	INIT_FILE = $(if $(shell ls init_script | grep -w $(BRANCH_INIT_FILE)),$(BRANCH_INIT_FILE),default)
+	INIT_FILE = $(if $(shell if [ -e init_script/$(BRANCH_INIT_FILE) ] ; then echo "file exist"; fi),$(BRANCH_INIT_FILE),default)
 endif
 
 ifdef KERNEL_DEBUG
