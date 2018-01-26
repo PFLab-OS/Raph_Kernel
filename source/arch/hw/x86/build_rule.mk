46d5a7a10d1dd1b18899dbf4595d679b74e7de97
liva <sap.pcmail@gmail.com>
2018-01-27 02:38:10 +0900

    solve network problem

--- source/arch/hw/x86/build_rule.mk
+++ source/arch/hw/x86/build_rule.mk
@@ -42,6 +42,7 @@ run:
 	$(MAKE) qemuend
 
 qemurun: image
+	sudo service networking restart
 	sudo qemu-system-x86_64 \
 		-cpu qemu64 -smp 8 -machine q35 -clock hpet \
 		-monitor telnet:127.0.0.1:1235,server,nowait \
